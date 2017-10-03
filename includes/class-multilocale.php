89dc42aa63183427ab5e43a77ff983912ba5f5ea
Barry Ceelen <barry@multipop.org>
2017-10-03 19:08:04 +0200

    Use parenthesis when instantiating the class

--- includes/class-multilocale.php
+++ includes/class-multilocale.php
@@ -77,7 +77,7 @@ class Multilocale {
 
 		// If the single instance hasn't been set, set it now.
 		if ( null === self::$instance ) {
-			self::$instance = new self;
+			self::$instance = new self();
 		}
 		return self::$instance;
 	}
